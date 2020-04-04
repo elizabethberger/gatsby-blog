cbf2fa136abaa3128bd01b66c572a5d6de9177ea
Elzbieta Szulc <elszulc@wp.pl>
2020-04-04 20:08:23 +0200

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,46 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import Header from './header'
 import '../styles/index.scss'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
+import { Row, Col } from 'reactstrap'
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ authorImageFluid, children, pageTitle, postAuthor }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div className="container" id="content"
-      >
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
+    `}
+    render={data => (
+      <>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="container" id="content">
+          <h1>{pageTitle}</h1>
+          <Row>
+            <Col md="8">{children}</Col>
+            <Col md="4">
+              <Sidebar author={postAuthor} authorFluid={authorImageFluid} />
+            </Col>
+          </Row>
+        </div>
+        <Footer />
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
