a77a86d826083861983ec2595e9c75f03abad09e
Elzbieta Szulc <elszulc@wp.pl>
2020-04-05 16:49:57 +0200

    Fix problems with remark

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,87 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const _ = require("lodash")
+// const path = require("path")
 
-// You can delete this file if you're not using it
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   return new Promise((resolve, reject) => {
+//     const pages = []
+//     const blogPost = path.resolve("src/templates/blog-post.js")
+//     const tagPages = path.resolve("src/templates/tag-page.js")
+//     graphql(
+//       `
+//         {
+//           allMarkdownRemark(
+//             limit: 1000
+//             filter: { frontmatter: { draft: { ne: true } } }
+//           ) {
+//             edges {
+//               node {
+//                 fields {
+//                   slug
+//                 }
+//                 frontmatter {
+//                   tags
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       `
+//     ).then(result => {
+//       if (result.errors) {
+//         console.log(result.errors)
+//         resolve()
+//         // reject(result.errors);
+//       }
+
+//       // Create blog posts pages.
+
+//       // Tag pages.
+//       let tags = []
+    
+//       tags = _.uniq(tags)
+//       tags.forEach(tag => {
+//         const tagPath = `/tags/${_.kebabCase(tag)}/`
+//         createPage({
+//           path: tagPath,
+//           component: tagPages,
+//           context: {
+//             tag,
+//           },
+//         })
+//       })
+
+//       resolve()
+//     })
+//   })
+// }
+
+// //exports.postBuild = require('./post-build')
+
+// // Add custom url pathname for blog posts.
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
+
+//   if (node.internal.type === `File`) {
+//     const parsedFilePath = path.parse(node.absolutePath)
+//     const slug = `/${parsedFilePath.dir.split("---")[1]}/`
+//     createNodeField({ node, name: `slug`, value: slug })
+//   } else if (
+//     node.internal.type === `MarkdownRemark` &&
+//     typeof node.slug === "undefined"
+//   ) {
+//     const fileNode = getNode(node.parent)
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: fileNode.fields.slug,
+//     })
+//     if (node.frontmatter.tags) {
+//       const tagSlugs = node.frontmatter.tags.map(
+//         tag => `/tags/${_.kebabCase(tag)}/`
+//       )
+//       createNodeField({ node, name: `tagSlugs`, value: tagSlugs })
+//     }
+//   }
+// }
