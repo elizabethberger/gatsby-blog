f0ce638ad9234dccdd8fe7ebc8d6e81dff9b4b7b
Elzbieta Szulc <elszulc@wp.pl>
2020-04-23 21:23:27 +0200

    Add tags to the posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,16 @@ exports.onCreateNode = ({ node, actions }) => {
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
 
-  return graphql(`
+  // Page templates
+  const templates = {
+    post: path.resolve('src/templates/single-post.js'),
+    postList: path.resolve('src/templates/post-list.js'),
+    tag: path.resolve('src/templates/tag-posts.js'),
+    tagsPage: path.resolve('src/templates/tags-page.js'),
+    authorPosts: path.resolve('src/templates/author-posts.js'),
+  }
+
+  const res = await graphql(`
     {
       allMarkdownRemark {
         edges {
@@ -33,20 +42,43 @@ exports.createPages = async ({ actions, graphql }) => {
         }
       }
     }
-  `).then(res => {
-        if(res.errors) return Promise.reject(res.errors)
+  `)
+
+  if (res.errors) return Promise.reject(res.errors)
+
+  // Extracting all posts from res
+  const posts = res.data.allMarkdownRemark.edges
+
   // Create single post pages
   posts.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: templates.post,
       context: {
+        // Passing slug for template to use to fetch the post
         slug: node.fields.slug,
-        // Find author imageUrl
+        // Find author imageUrl from author array and pass it to template
         imageUrl: authors.find(x => x.name === node.frontmatter.author)
           .imageUrl,
       },
     })
   })
-})
+
+  // Create posts pagination pages
+  const postsPerPage = 2
+  const numberOfPages = Math.ceil(posts.length / postsPerPage)
+
+  // Get all tags
+  let tags = []
+  _.each(posts, edge => {
+    if (_.get(edge, 'node.frontmatter.tags')) {
+      tags = tags.concat(edge.node.frontmatter.tags)
+    }
+  })
+
+  let tagPostCounts = {} // { tutorial: 2, design: 1}
+  tags.forEach(tag => {
+    // Or 0 cause it might not exist yet
+    tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+  })
 }
---
+++ src/util/authors.js
@@ -0,0 +1,12 @@
+const authors = [
+  {
+    name: 'Ela Szulc',
+    imageUrl: 'zdjecie2.jpg',
+    bio:
+      'I am a beginner Frontend programmer. The website was created from her passion for programming and learning new things.',
+    facebook: 'https://www.facebook.com/elzbieta.20',
+    twitter: 'https://twitter.com/szulc_elzbieta',
+    linkedin: 'https://www.linkedin.com/in/el%C5%BCbieta-szulc-a27075167/',
+  }
+]
+module.exports = authors
