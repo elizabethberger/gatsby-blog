47fcd90e8152527ee3b4c6ac7966d45e292b65c1
Elzbieta Szulc <elszulc@wp.pl>
2020-05-03 20:14:25 +0200

    Add author page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,34 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import authors from "../util/authors"
+import { Button, Card, CardText, CardBody, CardTitle, Row } from "reactstrap"
+import MyImage from "../images/ja_2.jpg"
+import { slugify } from "../util/utilityFunctions"
 
 const AboutPage = () => (
-  <Layout pageTitle="About me">
-    <SEO title="About page" keywords={[`gatsby`, `application`, `react`]} />
-    <p>Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+  <Layout pageTitle="Who Am I?">
+    <SEO title="Author" keywords={[`gatsby`, `application`, `react`]} />
+    <Row className="mb-4">
+      <div className="col-md-5">
+        <img src={MyImage} style={{ maxWidth: "100%" }} alt="Ela profile" />
+      </div>
+      <div className="col-md-8">
+        <Card style={{ minHeight: "100%" }}>
+          <CardBody>
+            <CardTitle>{authors[0].name}</CardTitle>
+            <CardText>{authors[0].bio}</CardText>
+            <Button
+              classname="text-uppercase"
+              color="primary"
+              href={`/author/${slugify(authors[0].name)}`}
+            >
+              Zobacz posty
+            </Button>
+          </CardBody>
+        </Card>
+      </div>
+    </Row>
   </Layout>
 )
 
--- src/util/authors.js
+++ src/util/authors.js
@@ -3,7 +3,7 @@ const authors = [
     name: 'Ela Szulc',
     imageUrl: 'zdjecie2.jpg',
     bio:
-      'I am a beginner Frontend programmer. The website was created from her passion for programming and learning new things.',
+      'Cześć! Strona powstała z pasji do programowania. Marzę aby w przyszłości zdobyć zawód frontend developera. W wolnych chwilach: ćwiczę, gotuję wegańskie potrawy i czytam co mi wpadnie w ręce :)',
     facebook: 'https://www.facebook.com/elzbieta.20',
     twitter: 'https://twitter.com/szulc_elzbieta',
     linkedin: 'https://www.linkedin.com/in/el%C5%BCbieta-szulc-a27075167/',
