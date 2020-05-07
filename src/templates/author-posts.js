14d178206e0118c01c9bfeefd663ed1b9d3f33b9
Elzbieta Szulc <elszulc@wp.pl>
2020-05-07 20:48:43 +0200

    Add author post page

--- src/templates/author-posts.js
+++ src/templates/author-posts.js
@@ -9,9 +9,7 @@ const authorPosts = ({ data, pageContext }) => {
   const author = authors.find(x => x.name === pageContext.authorName)
   const pageHeader = `${totalCount} Posts by: ${pageContext.authorName}`
 
-}
-
-return (
+  return (
     <Layout
       pageTitle={pageHeader}
       postAuthor={author}
@@ -32,4 +30,44 @@ return (
     </Layout>
   )
 }
+
+export const authorQuery = graphql`
+  query($authorName: String!, $imageUrl: String!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { author: { eq: $authorName } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM Do YYYY")
+            author
+            tags
+            image {
+              childImageSharp {
+                fluid(maxWidth: 650) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+    file(relativePath: { eq: $imageUrl }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 export default authorPosts
